f42eb4691c937e3066dd96bbee5cfcf284f1f7a7
Patrick Arlt <patrick.arlt@gmail.com>
2015-07-28 11:58:20 -0700

    update

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
 // require leaflet
 var L = require('leaflet');
 
+// require esri-leaflet
+var esri = require('esri-leaflet');
+
+// require esri-leaflet-geocoder
+var geocoding = require('esri-leaflet-geocoder');
+
 // since leaflet is bundled into the browserify package it won't be able to detect where the images
 // solution is to point it to where you host the the leaflet images yourself
 L.Icon.Default.imagePath = 'http://cdn.leafletjs.com/leaflet-0.7.3/images';
 
-// require heatmap and cluster plugins
-// browserify will include them im the bundle and the plugins will be attached to window.L rather then return the class
-require('leaflet.heat');
-require('leaflet.markercluster');
-
-// require esri-leaflet
-var Esri = require('esri-leaflet');
-var ClusteredFeatureLayer = require('esri-leaflet-clustered-feature-layer');
-var HeatmapFeatureLayer = require('esri-leaflet-heatmap-feature-layer');
-
-// start coding!
+// create map
 var map = L.map('map').setView([45.526, -122.667], 15);
-var tiles = new Esri.BasemapLayer('Topographic').addTo(map);
 
-var features = new Esri.FeatureLayer('http://services.arcgis.com/rOo16HdIMeOBI4Mb/arcgis/rest/services/Trimet_Transit_Stops/FeatureServer/0').addTo(map);
-// var clusters = new ClusteredFeatureLayer('http://services.arcgis.com/rOo16HdIMeOBI4Mb/arcgis/rest/services/Trimet_Transit_Stops/FeatureServer/0').addTo(map);
-// var heatmap = new HeatmapFeatureLayer('http://services.arcgis.com/rOo16HdIMeOBI4Mb/arcgis/rest/services/Trimet_Transit_Stops/FeatureServer/0').addTo(map);
+// add basemap
+esri.basemapLayer('Topographic').addTo(map);
+
+// add layer
+esri.featureLayer({
+  url: '//services.arcgis.com/uCXeTVveQzP4IIcx/arcgis/rest/services/gisday/FeatureServer/0/'
+}).addTo(map);
+
+// add search control
+geocoding.geosearch({
+  providers: [
+    geocoding.arcgisOnlineProvider(),
+    geocoding.featureLayerProvider({
+      url: '//services.arcgis.com/uCXeTVveQzP4IIcx/arcgis/rest/services/gisday/FeatureServer/0/',
+      searchFields: ['Name', 'Organization'],
+      label: 'GIS Day Events',
+      bufferRadius: 20000,
+      formatSuggestion: function (feature) {
+        return feature.properties.Name + ' - ' + feature.properties.Organization;
+      }
+    })
+  ]
+}).addTo(map);
